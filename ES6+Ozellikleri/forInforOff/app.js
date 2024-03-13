86d1738d6ab9c426bc0882855afddb1c60999ec2
Hilmi Ozyurt <hilmi.ozyrt@gmail.com>
2024-03-14 01:04:26 +0300

    es6+

---
+++ ES6+Ozellikleri/forInforOff/app.js
@@ -0,0 +1,40 @@
+const person = {
+    name: "Muhammed Hilmi",
+    age: 25,
+    salary: 3000,
+};
+
+const langs = ["Python","Java","C++","Php"];
+
+const name = "Muhammed";
+
+// For in
+// Object
+// for (let prop in person){
+//     console.log(prop, person[prop]);
+// }
+// Array
+// for (let index in langs){
+//     console.log(index,langs[index]);
+// }
+// String
+// for (let index in name){
+//     console.log(index, name[index]);
+// }
+
+// FOR OFF
+
+// Object olmuyor.!
+// for (let value of person){
+//     console.log(value);
+// }
+// LİST
+// for(let value of langs){
+//     console.log(value);
+// }
+
+// String
+// for(let char of name){
+//     console.log(char);
+// }
+
